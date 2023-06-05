490bd6363fb1fa56b1e3512e5c7bc6b8ca7e0df5
Fernando Ciballero <fciballero@vmox.com>
2023-06-05 14:37:22 -0300

    add logic for CRUD actions

---
+++ README.md
@@ -0,0 +1,9 @@
+# Basic Todo App
+
+This is a pretty basic TO DO app created using [Vue JS Framework](https://vuejs.org/)
+
+# Screenshots
+
+![todo-list](./assets/todo-list.jpg)
+![create-todo](./assets/create-todo.jpg)
+![edit-todo](./assets/edit-todo.jpg)
--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
 const app = Vue.createApp({
   data() {
     return {
-      message: 'Todo App'
+      message: 'Todo App',
+      task: null,
+      todosList: [
+        { task: 'Have breakfast', edit: false },
+        { task: 'Clean my room', edit: false },
+        { task: 'Go to the gym', edit: false },
+        { task: 'Prepare a meal', edit: false }
+      ]
+    }
+  },
+  methods: {
+    addTodo(){
+      if(this.task != null){
+        let task = {
+          task: this.task
+        }
+        this.todosList.push(task)
+        this.task = null
+      }
+    },
+    showEditForm(index){
+      this.todosList[index].edit = true
+      this.task = this.todosList[index].task
+    },
+
+    editTodo(index){
+      this.disableFormInput = true
+      this.todosList[index].task = this.task
+      this.todosList[index].edit = false
+      this.task = null
+    },
+    deleteTodo(index){
+      this.todosList.splice(index, 1);
     }
   }
 })
